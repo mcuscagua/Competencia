c4268b42bb58324c7c6d3715d4159c6d26d9c919
mcuscagua <36216379+mcuscagua@users.noreply.github.com>
2019-09-14 00:17:11 -0500

    up

--- Count/Diana/FSA.py
+++ Count/Diana/FSA.py
@@ -10,6 +10,10 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+from sklearn.feature_selection import chi2
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.metrics import roc_auc_score, mean_squared_error
 from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
@@ -33,6 +37,8 @@ from sklearn.feature_selection import SelectKBest, SelectPercentile
 from sklearn.model_selection import train_test_split
 
 num_sim_models = 1000
+
+Categorical = np.array([str(x) for x in X.dtypes.values])
 
 # Variables per model
 MutInfoVar = np.array([])
@@ -62,4 +68,33 @@ for feature in X.columns:
 
 roc_values = pd.Series(roc_values)
 roc_values.index = Xt.columns
-roc_values.sort_values(ascending=False)
+roc_values = roc_values.sort_values(ascending=False)
+
+MSEDef = roc_values.index[roc_values > 0.5].values
+
+# Fisher score
+
+f_score = chi2(X[X.columns[Categorical == 'int64'].values], Y)
+pvalues = pd.Series(f_score[1])
+pvalues.index = X.columns[Categorical == 'int64'].values
+pvalues = pvalues.sort_values(ascending= True)
+
+FisScoDef = pvalues.index[pvalues <= 0.05].values
+
+# Step Forward Selection
+
+N_feat = [15, 16, 17, 18, 19, 20, 21, 22]
+selected_features = []
+for i in N_feat:
+    sfs1 = SFS(RandomForestClassifier(n_estimators=200),
+               n_jobs=-1,
+               k_features=i,
+               forward=True,
+               floating=False,
+               verbose=2,
+               scoring='roc_auc',
+               cv=5)
+
+    sfs1 = sfs1.fit(X,Y)
+    selected_feat = X.columns[list(sfs1.k_feature_idx_)]
+    selected_features = np.concatenate([selected_features, selected_feat.values])
